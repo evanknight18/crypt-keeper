af60f32467d2537b65571cbbaa8819cd4c55024d
Evan <evanknight18@gmail.com>
2023-05-23 20:07:24 -0600

    added middleware and routes

--- server.js
+++ server.js
@@ -24,3 +24,16 @@ app.use(session(sess));
 // Set up Handlebars.js engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+// Add routes, both API and view
+app.use(routes);
+
+// Start server after DB connection
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  });
