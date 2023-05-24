4319b26ea80d524097812bb2840b3169036406b3
Evan <evanknight18@gmail.com>
2023-05-23 20:59:39 -0600

    added bcrypt requirement to user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
+const bcrypt = require('bcryptjs');
 
 // POST /api/users/signup
 router.post('/signup', async (req, res) => {
