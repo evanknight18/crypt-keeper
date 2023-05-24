656496ec1ad32b971a5a501a981cc6272b9f175b
Evan <evanknight18@gmail.com>
2023-05-23 19:59:12 -0600

    Sequelize and Handlebars engine setup

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ const exphbs = require('express-handlebars'); // Import Handlebars
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up sessions with Sequelize
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
+
+// Set up Handlebars.js engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
