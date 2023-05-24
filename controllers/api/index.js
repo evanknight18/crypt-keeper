659a298a42563b3c315b2e6a09e60e176efa84e0
Evan <evanknight18@gmail.com>
2023-05-23 20:21:29 -0600

    added import routes

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// Import routes
+const userRoutes = require('./userRoutes');
+const portfolioRoutes = require('./portfolioRoutes');
+const cryptoRoutes = require('./cryptoRoutes');
+
+// Set up routes
+router.use('/users', userRoutes);
+router.use('/portfolio', portfolioRoutes);
+router.use('/crypto', cryptoRoutes);
+
+module.exports = router;
