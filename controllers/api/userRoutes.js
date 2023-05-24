2773cf5f8599d25b24c570142de73406c331508d
Evan <evanknight18@gmail.com>
2023-05-23 20:48:11 -0600

    user sign up route and logout set up, auth and user update await logic

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,40 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// POST /api/users/signup
+router.post('/signup', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// POST /api/users/login
+router.post('/login', async (req, res) => {
+    // Authentication logic here
+});
+
+// POST /api/users/logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// PUT /api/users/:id (for updating user details)
+router.put('/:id', withAuth, async (req, res) => {
+    // User update logic here
+});
+
+module.exports = router;
