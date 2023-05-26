e21909e30771519583663c95604e33f8566dfaab
Evan <evanknight18@gmail.com>
2023-05-25 20:32:12 -0600

    blank json files in seeds created

--- controllers/api/portfolioRoutes.js
+++ controllers/api/portfolioRoutes.js
@@ -16,6 +16,27 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+// Update a portfolio entry
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const portfolioData = await Portfolio.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.userId,
+            },
+        });
+
+        if (!portfolioData[0]) {
+            res.status(404).json({ message: 'No portfolio entry found with this id' });
+            return;
+        }
+
+        res.status(200).json(portfolioData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 
 module.exports = router;
