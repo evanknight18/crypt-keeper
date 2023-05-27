daa66d538c607270fbfae7a351f0fc2129d5ab84
Evan <evanknight18@gmail.com>
2023-05-27 13:30:45 -0600

    portfolio routes updated

--- controllers/api/portfolioRoutes.js
+++ controllers/api/portfolioRoutes.js
@@ -1,42 +1,68 @@
+const axios = require('axios');
 const router = require('express').Router();
 const { Portfolio } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Create a new portfolio entry
+// Get user's portfolio with real-time prices
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const portfolioData = await Portfolio.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    const portfolio = portfolioData.map((coin) => coin.get({ plain: true }));
+
+    for (let coin of portfolio) {
+      const response = await axios.get(`INSERT API URL`);
+      coin.price = response.data[coin.name].usd;
+    }
+
+    res.json(portfolio);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add a coin to user's portfolio
 router.post('/', withAuth, async (req, res) => {
-    try {
-        const newPortfolio = await Portfolio.create({
-            ...req.body,
-            user_id: req.session.userId,
-        });
+  try {
+    // Fetch the real-time price of the coin from the CoinGecko API
+    const response = await axios.get(`INSERT API URL`);
+    const price = response.data[req.body.name].usd;
 
-        res.status(200).json(newPortfolio);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    const newCoin = await Portfolio.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      price: price,
+    });
+
+    res.status(200).json(newCoin);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-// Update a portfolio entry
-router.put('/:id', withAuth, async (req, res) => {
-    try {
-        const portfolioData = await Portfolio.update(req.body, {
-            where: {
-                id: req.params.id,
-                user_id: req.session.userId,
-            },
-        });
+// Delete a coin from user's portfolio
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const portfolioData = await Portfolio.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-        if (!portfolioData[0]) {
-            res.status(404).json({ message: 'No portfolio entry found with this id' });
-            return;
-        }
-
-        res.status(200).json(portfolioData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!portfolioData) {
+      res.status(404).json({ message: 'No portfolio found with this id!' });
+      return;
     }
+
+    res.status(200).json(portfolioData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
 
 module.exports = router;
