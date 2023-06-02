afe70e5c0c6802e6d2861786050808155a9db8c6
Aaron <attanner86@gmail.com>
2023-06-01 23:54:28 -0600

    gpt

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
 const {User, Portfolio, Coin} = require('../models');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 router.get('/', async (req, res) => {
   try {
+    
+    let coins = ['bitcoin', 'ethereum'];
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `My cryptocurrency portfolio holds ${coins}. As of the year 2023, give me predictions for the coins in my portfolio and recent news for each coin. Finally, suggest one cryptocurrency that is worth researching more about.`,
+        temperature: 1.2,
+        max_tokens: 2048,
+        // top_p: 1.0,
+        n: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["\"\"\""],
+      });
+      const resGPT = response.data.choices[0].text;
 
     const portfolioData = await Portfolio.findAll({
       where: { user_id: req.session.user_id },
@@ -16,7 +38,7 @@ router.get('/', async (req, res) => {
     console.log(portfolio);
 
     res.render('homepage', {
-      ...portfolio[0],
+      ...portfolio[0], resGPT,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -32,5 +54,23 @@ router.get('/login', (req, res) => {
     // }
     res.render('login');
 });
+
+// async function getGPT(...coins){
+//   const response = await openai.createCompletion({
+//   model: "text-davinci-003",
+//   prompt: `My cryptocurrency portfolio holds ${coins}. As of the year 2023, give me predictions for the coins in my portfolio and recent news for each coin. Finally, suggest one cryptocurrency that is worth researching more about.`,
+//   temperature: 1.2,
+//   max_tokens: 2048,
+//   // top_p: 1.0,
+//   n: 1,
+//   frequency_penalty: 0.0,
+//   presence_penalty: 0.0,
+//   stop: ["\"\"\""],
+// });
+// // const data = await response.json();
+// let resGPT = response.data.choices[0].text;
+// console.log(resGPT);
+// return resGPT;
+// }
 
 module.exports = router;
